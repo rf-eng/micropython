90a8c69cbfcbc1b6fbbe32a986639786462e776c
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2020-11-30 12:27:07 +0100

    use new name for submodule upyscripts

--- ports/stm32/boards/ELITEBOARD_H743XI/manifest.py
+++ ports/stm32/boards/ELITEBOARD_H743XI/manifest.py
@@ -1,3 +1,3 @@
 include("$(PORT_DIR)/boards/manifest.py")
-freeze("$(MPY_DIR)/extmod/upyscripts_eliteboard/adafruit_drivers/lib")
-freeze("$(MPY_DIR)/extmod/upyscripts_eliteboard/adafruit_drivers", ("adafruit_hts221.py", "adafruit_lis3mdl.py", "adafruit_lps2x.py", "adafruit_lsm6ds.py"))
+freeze("$(MPY_DIR)/extmod/eliteboard_upyscripts/adafruit_drivers/lib")
+freeze("$(MPY_DIR)/extmod/eliteboard_upyscripts/adafruit_drivers", ("adafruit_hts221.py", "adafruit_lis3mdl.py", "adafruit_lps2x.py", "adafruit_lsm6ds.py"))
