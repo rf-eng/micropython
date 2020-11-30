690b067888e25b70f3c23fb907f8281ec3cfe1ec
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2020-11-30 12:04:17 +0100

    freeze adafruit drivers and remove frozen testfile

--- ports/stm32/boards/ELITEBOARD_H743XI/manifest.py
+++ ports/stm32/boards/ELITEBOARD_H743XI/manifest.py
@@ -1,4 +1,5 @@
 include("$(PORT_DIR)/boards/manifest.py")
-freeze("modules")
+#freeze("modules")
+include("$(MPY_DIR)/extmod/upyscripts_eliteboard/adafruit_drivers/manifest.py")
 #freeze("./modules", ("freezetest.py"))
 #include("$(MPY_DIR)/extmod/webrepl/manifest.py")
